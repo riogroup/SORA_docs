3a2b29602ae20182e8aa2b9d0699b0df80638fbd
Bruno Morgado <morgado.fis@gmail.com>
2021-06-15 09:32:50 -0300

    sora code updated to v0.2

--- sora/__init__.py
+++ sora/__init__.py
@@ -1,10 +1,10 @@
 from .body import Body
 from .ephem import EphemKernel, EphemPlanete, EphemJPL, EphemHorizons
 from .lightcurve import LightCurve
-from .observer import Observer
+from .observer import Observer, Spacecraft
 from .star import Star
 from .occultation import Occultation
 
-__version__ = '0.2dev'
+__version__ = '0.2'
 
 print(f'SORA version: {__version__}')

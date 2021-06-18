981682cec1e175f8259a5de94746beb2d70e68ad
Bruno Morgado <Bmorgado19@users.noreply.github.com>
2021-06-17 21:53:55 -0300

    Update conf.py with pygments style

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ language = None
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'friendly'
 
 
 # -- Options for HTML output -------------------------------------------------
